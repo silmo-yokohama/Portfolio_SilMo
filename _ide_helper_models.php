052ea2d66e9cba6e3b45f7b937425612f2009f40
silmo-yokohama <silmo.yokohama@gmail.com>
2024-01-20 04:15:28 +0900

    IDEヘルパー導入

---
+++ _ide_helper_models.php
@@ -0,0 +1,160 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\Skill
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property int $is_deleted
+ * @property string|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Work> $works
+ * @property-read int|null $works_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Skill newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Skill newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Skill query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Skill whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Skill whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Skill whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Skill whereIsDeleted($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Skill whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Skill whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Skill whereUpdatedAt($value)
+ */
+	class Skill extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Tag
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property int $is_deleted
+ * @property string|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Work> $works
+ * @property-read int|null $works_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereIsDeleted($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tag whereUpdatedAt($value)
+ */
+	class Tag extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property mixed $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Work
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $image_path
+ * @property string|null $url
+ * @property string|null $content
+ * @property int $is_deleted
+ * @property string|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\WorkCategory> $categories
+ * @property-read int|null $categories_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Skill> $skills
+ * @property-read int|null $skills_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Tag> $tags
+ * @property-read int|null $tags_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Work newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Work newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Work query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Work whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Work whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Work whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Work whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Work whereImagePath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Work whereIsDeleted($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Work whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Work whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Work whereUrl($value)
+ */
+	class Work extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\WorkCategory
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property int $is_deleted
+ * @property string|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Work> $works
+ * @property-read int|null $works_count
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkCategory newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkCategory newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkCategory query()
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkCategory whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkCategory whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkCategory whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkCategory whereIsDeleted($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkCategory whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkCategory whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|WorkCategory whereUpdatedAt($value)
+ */
+	class WorkCategory extends \Eloquent {}
+}
+
